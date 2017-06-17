76a561f0a70ac57d6cc585561e7450fda3eaeb98
mohamedou <mohamedou.ab@gmail.com>
2017-06-17 16:22:34 +0200

    first commit

---
+++ pluginRelease/src/main/java/mvn/rease/pluginRelease/App.java
@@ -0,0 +1,13 @@
+package mvn.rease.pluginRelease;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
